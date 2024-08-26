b271afddb5ceae43f240a1cbdccb8daa42aaa4e6
Vinay@itworks <vinay.poul@infotech.works>
2024-08-26 16:14:28 +0530

    26 Aug 2024
    Minor change exception statement.

--- Questions/practical_question/q1retry.rb
+++ Questions/practical_question/q1retry.rb
@@ -57,7 +57,7 @@ loop do
   # puts num1, num2
   if num1.to_i == 0 || num2.to_i == 0
     unless num1 == "0" || num2 == "0"
-      puts "Operations with string and zero not performed"
+      puts "Operations with string not performed."
       redo
     end
   end
